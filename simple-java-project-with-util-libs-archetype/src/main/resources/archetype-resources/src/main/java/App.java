798bc55d37ba8a1bf4315a827407e252453cd69d
akiraly <kiralyattila.hu@gmail.com>
2013-03-31 12:30:03 +0200

    Add archetype for creating a simple java project with predefined generic dependencies.

---
+++ simple-java-project-with-util-libs-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,8 @@
+import javax.annotation.Nonnull;
+
+@Nonnull
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ simple-java-project-with-util-libs-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+@Nonnull
+public class AppTest {
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
