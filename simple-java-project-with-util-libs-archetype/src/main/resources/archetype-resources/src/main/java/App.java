08e2db97d7cb18ce11affdc7ae9ee40e08aaa42d
akiraly <kiralyattila.hu@gmail.com>
2013-04-24 23:23:20 +0200

    Enhance default classes generated by archetype.

--- simple-java-project-with-util-libs-archetype/src/main/resources/archetype-resources/src/main/java/App.java
+++ simple-java-project-with-util-libs-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,8 +1,18 @@
+package $$package;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Nonnull
 public class App {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		checkArgument(args != null, "Expected not null arguments.");
+		LOGGER.info("Hello World!");
 	}
 }
--- simple-java-project-with-util-libs-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ simple-java-project-with-util-libs-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,9 +1,19 @@
+package $$package;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Nonnull
 public class AppTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
+
 	@Test
 	public void testApp() {
-		assertTrue(true);
+		LOGGER.error("This test will fail.");
+		Assert.fail();
 	}
 }
