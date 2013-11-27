b30a359d2a43f2913ab22b636dd3c4a5572508b4
akiraly <kiralyattila.hu@gmail.com>
2013-11-27 22:19:19 +0100

    Update hibernate version.

--- uow/src/main/java/com/github/akiraly/uow/Uow.java
+++ uow/src/main/java/com/github/akiraly/uow/Uow.java
@@ -1,5 +1,7 @@
 package com.github.akiraly.uow;
 
+import static com.github.akiraly.ver4j.Verify.argNotNull;
+
 import javax.annotation.Nonnull;
 import javax.persistence.Entity;
 
@@ -12,4 +14,18 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 public class Uow extends AbstractPersistable<Long> {
 	private static final long serialVersionUID = -6140188419595214529L;
 
+	private final String user;
+
+	public Uow(String user) {
+		this.user = argNotNull(user, "user");
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	@Override
+	public String toString() {
+		return "Uow [getUser()=" + getUser() + ", getId()=" + getId() + "]";
+	}
 }
