537a02cbe29efafb98ca7cb5f338d9707be4bb62
akiraly <kiralyattila.hu@gmail.com>
2014-03-24 21:29:07 +0100

    Simplify TimeConverter with Java8 syntax.

--- db4j/src/main/java/com/github/akiraly/db4j/converter/TimeConverter.java
+++ db4j/src/main/java/com/github/akiraly/db4j/converter/TimeConverter.java
@@ -7,35 +7,29 @@ import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.Optional;
-import java.util.function.Function;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 @Nonnull
 public class TimeConverter {
-	private static final Function<OffsetDateTime, Date> offsetDateTime2DateFunction = offsetDateTime -> Date
-			.from(offsetDateTime.toInstant());
-
-	private static final Function<Date, OffsetDateTime> date2UtcOffsetDateTimeFunction = date -> OffsetDateTime
-			.ofInstant(date.toInstant(), ZoneOffset.UTC);
-
 	public OffsetDateTime date2UtcDateTime(Date date) {
-		return date2UtcOffsetDateTimeFunction.apply(argNotNull(date, "date"));
+		return OffsetDateTime
+				.ofInstant(argNotNull(date, "date").toInstant(), ZoneOffset.UTC);
 	}
 
 	public Optional<OffsetDateTime> date2OptUtcDateTime(@Nullable Date date) {
-		return ofNullable(date).map(date2UtcOffsetDateTimeFunction);
+		return ofNullable(date).map(dt -> date2UtcDateTime(dt));
 	}
 
 	@Nullable
 	public Date dateTime2Date(Optional<OffsetDateTime> offsetDateTime) {
 		return argNotNull(offsetDateTime, "offsetDateTime").map(
-				offsetDateTime2DateFunction).orElse(null);
+				odt -> dateTime2Date(odt)).orElse(null);
 	}
 
 	public Date dateTime2Date(OffsetDateTime offsetDateTime) {
-		return offsetDateTime2DateFunction.apply(argNotNull(offsetDateTime,
-				"offsetDateTime"));
+		return Date.from(argNotNull(offsetDateTime, "offsetDateTime")
+				.toInstant());
 	}
 }
