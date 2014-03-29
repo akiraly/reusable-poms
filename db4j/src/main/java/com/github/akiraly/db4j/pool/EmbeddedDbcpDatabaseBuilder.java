3297a07d78ef1afbed9fe6498b6ebd34ddf326ec
akiraly <kiralyattila.hu@gmail.com>
2014-03-29 20:26:26 +0100

    Update spring to latest stable (4.0.3). Drop now not needed custom db builder.

--- db4j/src/main/java/com/github/akiraly/db4j/pool/EmbeddedDbcpDatabaseBuilder.java
+++ db4j/src/main/java/com/github/akiraly/db4j/pool/EmbeddedDbcpDatabaseBuilder.java
@@ -2,11 +2,12 @@ package com.github.akiraly.db4j.pool;
 
 import javax.annotation.Nonnull;
 
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
 @Nonnull
-public class EmbeddedDbcpDatabaseBuilder extends
-		ConfigurableEmbeddedDatabaseBuilder {
+public class EmbeddedDbcpDatabaseBuilder extends EmbeddedDatabaseBuilder {
 
 	public EmbeddedDbcpDatabaseBuilder() {
-		super(new DbcpDataSourceFactory());
+		setDataSourceFactory(new DbcpDataSourceFactory());
 	}
 }
